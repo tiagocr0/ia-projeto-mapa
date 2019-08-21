ce5ae2aea0c3569596cc9beb781afd4b24a1b8d9
OsmarHofman <osmarjhdas@hotmail.com>
2019-08-21 20:25:12 -0300

    Projeto Criado

---
+++ EuroTour/src/main/java/eurotour/br/edu/ifsc/EuroTourApplication.java
@@ -0,0 +1,13 @@
+package eurotour.br.edu.ifsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroTourApplication.class, args);
+	}
+
+}
