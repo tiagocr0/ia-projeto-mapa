a02ede6df2da889124b8b6c37aff384b14ec7eb1
TIago Correia Ribeiro <correiaribeirotiago@gmail.com>
2019-08-21 11:33:13 -0300

    Projeto criado

---
+++ ia-euro-tour-ifsc/src/main/java/com/ia/euro/tour/ifsc/IaEuroTourIfscApplication.java
@@ -0,0 +1,13 @@
+package com.ia.euro.tour.ifsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaEuroTourIfscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaEuroTourIfscApplication.class, args);
+	}
+
+}
