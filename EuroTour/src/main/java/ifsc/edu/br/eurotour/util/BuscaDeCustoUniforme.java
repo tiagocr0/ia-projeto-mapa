00d7293406d6cb0301028b4e802947954d4046c7
Casa <Casa@Casa-PC>
2019-09-07 19:42:53 -0300

    Melhoria no método de busca uniforme

--- EuroTour/src/main/java/ifsc/edu/br/eurotour/util/BuscaDeCustoUniforme.java
+++ EuroTour/src/main/java/ifsc/edu/br/eurotour/util/BuscaDeCustoUniforme.java
@@ -1,6 +1,8 @@
 package ifsc.edu.br.eurotour.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import ifsc.edu.br.eurotour.model.grafo.Arco;
@@ -18,44 +20,62 @@ import ifsc.edu.br.eurotour.repository.BuscaCustoUniformeRepository;
  */
 public class BuscaDeCustoUniforme implements BuscaCustoUniformeRepository {
 
+	
+	
+public static final int VERTICE_COM_MENOR_DISTANCIA = 0;
+    
+	private List<Vertice> lVerticesAbertos = new ArrayList<>();
+	private List<Vertice> lVerticesExpandidos = new ArrayList<>();
+
 	@Override
-	public Caminho buscaCustoUniforme(Grafo g, Vertice inicial, Vertice destino) {
-		List<Vertice> lVerticesAbertos = new ArrayList<>();
-		reiniciarGrafo(g);
-		inicial.definirDistancia(0);
-		lVerticesAbertos.add(inicial);
-		double lMenorDistancia = 0.0;
-		Vertice lVerticeOrigem = new Vertice();
+    public Caminho buscaCustoUniforme(Grafo aGrafo, Vertice aInicial, Vertice aFinal) {
+        reiniciarGrafo(aGrafo);
+        aInicial.definirDistancia(0);
+        lVerticesAbertos.add(aInicial);
+   
+        lVerticesAbertos.addAll(aGrafo.obterVertices());
+        while (lVerticesAbertos.size() > 0) {
+//        	Oodena os vertices para deixar o com menor distancia na posição 0
+            ordernar(lVerticesAbertos);
+//          Remove o vertice com menor distancia
+            Vertice lVerticeOrigem = lVerticesAbertos.remove(VERTICE_COM_MENOR_DISTANCIA);
+//          Adiconar o vertice na lista dos vertices expandidos
+            lVerticesExpandidos.add(lVerticeOrigem);
+            
+            if (!lVerticeOrigem.equals(aFinal)) {
+                for (Arco lArco : lVerticeOrigem.obterArcos()) {
+                    Vertice lVerticeDestino = lArco.getDestino();
+                    double lDistanciaPorPeso = lArco.getPeso();
+                    if (lVerticeDestino.obterDistancia() > lVerticeOrigem.obterDistancia() + lDistanciaPorPeso) {
+                        lVerticeDestino.definirDistancia(lDistanciaPorPeso + lVerticeOrigem.obterDistancia());
+                        lVerticeDestino.setCaminho(lVerticeOrigem.getCaminho());
+                    }
+                }
+            } else {
+                return Caminho.converter(aGrafo, aFinal, lVerticeOrigem.obterDistancia());
+            }
+        }
+        return Caminho.converter(aGrafo, aFinal, aFinal.obterDistancia());
+    }
 
-		lVerticesAbertos.addAll(g.obterVertices());
-		while (lVerticesAbertos.size() > 0) {
-			lMenorDistancia = lVerticesAbertos.get(0).obterDistancia();
-			int indice = 0;
-			for (Vertice v : lVerticesAbertos) {
-				if (v.obterDistancia() < lMenorDistancia) {
-					lMenorDistancia = v.obterDistancia();
-					indice = lVerticesAbertos.indexOf(v);
-				}
-			}
+//	Ordena a lista de arcos em onderm crecsente
+    private static void ordernar(List<Vertice> aArcos) {
+        Collections.sort(aArcos, new CustomComparatorVertice());
+    }
 
-			lVerticeOrigem = lVerticesAbertos.remove(indice);
+//	Ordenação dos arcos por distancia.
+    private static class CustomComparatorVertice implements Comparator<Vertice> {
 
-			if (!lVerticeOrigem.equals(destino)) {
-				for (Arco lArco : lVerticeOrigem.obterArcos()) {
-					Vertice lVerticeDestino = lArco.getDestino();
-					double lDistanciaPorPeso = lArco.getPeso();
-					if (lVerticeDestino.obterDistancia() > lVerticeOrigem.obterDistancia() + lDistanciaPorPeso) {
-						lVerticeDestino.definirDistancia(lDistanciaPorPeso + lVerticeOrigem.obterDistancia());
-						lVerticeDestino.setCaminho(lVerticeOrigem.getCaminho());
-					}
-				}
-			} else {
-				return Caminho.converter(g, destino, lVerticeOrigem.obterDistancia());
-			}
-		}
-		return Caminho.converter(g, destino, lVerticeOrigem.obterDistancia());
-	}
+        @Override
+        public int compare(Vertice aVerticeI, Vertice aVerticeII) {
+            Double pesoI = aVerticeI.obterDistancia();
+            Double pesoII = aVerticeII.obterDistancia();
+            return pesoI.compareTo(pesoII);
+        }
 
+    }
+	
+//	Zera as informações dos vertices do gráfico.
 	private static void reiniciarGrafo(Grafo aGrafo) {
 		for (Vertice lVertice : aGrafo.obterVertices()) {
 			lVertice.zerarVisitas();
