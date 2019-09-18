dc19b4f6cbb3bab48f07cb39074e7b293ba749a4
TIago Correia Ribeiro <correiaribeirotiago@gmail.com>
2019-09-18 11:21:13 -0300

    Documentação das classes Front e FrontToBack finalizadas

--- EuroTour/src/main/java/ifsc/edu/br/eurotour/model/mapeamento/Front.java
+++ EuroTour/src/main/java/ifsc/edu/br/eurotour/model/mapeamento/Front.java
@@ -4,13 +4,8 @@ package ifsc.edu.br.eurotour.model.mapeamento;
  * Classe que recebe {@link FrontToBack} para formatá-la e receber do front-end
  * no formato exemplo:
  * 
- * {
- *  "front":{
- *     "algoritmo": 3,
- *	   "origem": "Albânia – Tirana",
- *	   "destino": "Bélgica – Bruxelas"
- *    }
- * }
+ * { "front":{ "algoritmo": 3, "origem": "Albânia – Tirana", "destino": "Bélgica
+ * – Bruxelas" } }
  * 
  * @author Tiago
  *
@@ -20,12 +15,15 @@ public class Front {
 
 	private FrontToBack front;
 
+	/**
+	 * Retorna a classe {@link FrontToBack},classe responsável para receber os dados
+	 * do Front-end
+	 * 
+	 * @return {@link FrontToBack} contendo as informações de algoritmo,origem e
+	 *         destino
+	 */
 	public FrontToBack getFront() {
 		return front;
-	}
-
-	public void setFront(FrontToBack front) {
-		this.front = front;
 	}
 
 }
--- EuroTour/src/main/java/ifsc/edu/br/eurotour/model/mapeamento/FrontToBack.java
+++ EuroTour/src/main/java/ifsc/edu/br/eurotour/model/mapeamento/FrontToBack.java
@@ -18,28 +18,32 @@ public class FrontToBack {
 
 	}
 
+	/**
+	 * Retorna o identificador do algoritmo que deverá ser usado para realizar a
+	 * busca
+	 * 
+	 * @return um inteiro que será o número do algoritmo da busca
+	 */
 	public int getAlgoritmo() {
 		return algoritmo;
 	}
 
-	public void setAlgoritmo(int algoritmo) {
-		this.algoritmo = algoritmo;
-	}
-
+	/**
+	 * Retorna o país de origem da busca que deverá ser realizada
+	 * 
+	 * @return uma String com o rótulo do país de origem
+	 */
 	public String getOrigem() {
 		return origem;
 	}
 
-	public void setOrigem(String origem) {
-		this.origem = origem;
-	}
-
+	/**
+	 * Retorna o país de destino da busca que deverá ser realizada
+	 * 
+	 * @return uma String com o rótulo do país de destino
+	 */
 	public String getDestino() {
 		return destino;
-	}
-
-	public void setDestino(String destino) {
-		this.destino = destino;
 	}
 
 	@Override
